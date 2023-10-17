531dfa57569efc56ce55368962f30a4f681ee52e
wiouxev <wiouxev@gmail.com>
2023-10-17 08:49:30 -0500

    Add files via upload

---
+++ winget_get-regUninstallKey.ps1
@@ -0,0 +1,23 @@
+﻿function Get-RegUninstallKey
+{
+	param (
+	[string]$DisplayName
+	)
+	$ErrorActionPreference = 'Continue'
+	$uninstallKeys = "registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall", "registry::HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+	$softwareTable =@()
+	
+	foreach ($key in $uninstallKeys)
+	{
+		$softwareTable += Get-Childitem $key | Get-ItemProperty | where displayname | Sort-Object -Property displayname
+	}
+	if ($DisplayName)
+	{
+		$softwareTable | where displayname -Like "*$DisplayName*"
+	}
+	else
+	{
+		$softwareTable | Sort-Object -Property displayname -Unique
+	}
+	
+}
